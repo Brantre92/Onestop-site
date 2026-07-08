a56276a38500738c15122c78511b3f26f2d75d81
Brantre92 <brandon.j.trejo@gmail.com>
2026-07-08 15:03:01 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-toggle.addEventListener('click', () => navLinks.classList.toggle('open'));
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => navLinks.classList.remove('open'));
-});
