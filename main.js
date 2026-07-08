c11909b9bf29d731f2fc372771dd8e5a0089c792
Brantre92 <brandon.j.trejo@gmail.com>
2026-07-08 15:23:23 -0600

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const btn=document.querySelector('.menu-btn');
-const menu=document.querySelector('.menu');
-btn?.addEventListener('click',()=>menu.classList.toggle('open'));
-document.querySelectorAll('.menu a').forEach(a=>a.addEventListener('click',()=>menu.classList.remove('open')));
-const obs=new IntersectionObserver(entries=>{entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')})},{threshold:.15});
-document.querySelectorAll('.reveal').forEach(el=>obs.observe(el));
